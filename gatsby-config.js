4c1ccbb60450f1a61508e7f5c4d16e0251d5d484
Thom Strub <thomstrub@gmail.com>
2023-02-10 21:04:43 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Puget Sound Pinhole Society`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
